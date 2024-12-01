fbba24ab70b7508515b62f803e7b0fd687d55423
Farihul Rouf <farihulrouf@gmail.com>
2024-12-01 12:10:07 +0700

    title:

--- app/controllers/scraper_controller.py
+++ app/controllers/scraper_controller.py
@@ -21,29 +21,56 @@ def get_amazon_products(url: str):
     soup = BeautifulSoup(response.text, 'html.parser')
 
     # Mengambil elemen produk
-    product_containers = soup.find_all('div', class_='s-product-image-container')
+    product_containers = soup.find_all('div', class_='s-result-item')  # Menggunakan kelas 's-result-item'
     if not product_containers:
         raise Exception("Elemen produk tidak ditemukan.")
     
     products = []
     for product in product_containers:
+        # Mencari judul produk
+        title_tag = product.find('span', class_='a-text-normal')
+        if not title_tag:
+            title_tag = product.find('h2', class_='a-size-mini')
+        if not title_tag:
+            title_tag = product.find('a', class_='a-link-normal')  # Coba tag <a> dengan class tertentu
+        if title_tag:
+            product_title = title_tag.get_text(strip=True)
+        else:
+            product_title = None
+        
+        # Jika judul kosong, lewati produk ini
+        if not product_title:
+            continue
+        
+        # Mengambil URL gambar
         img_tag = product.find('img')
         if img_tag and 'src' in img_tag.attrs:
             img_url = img_tag['src']
         else:
             img_url = None
         
+        # Mengambil link produk
         link_tag = product.find('a', class_='a-link-normal')
         if link_tag and 'href' in link_tag.attrs:
-            product_link = 'https://www.amazon.com' + link_tag['href']
+            product_link = 'https://www.amazon.com' + link_tag['href'] if link_tag['href'].startswith('/') else link_tag['href']
         else:
             product_link = None
         
         # Simpan data produk dalam dictionary
         product_data = {
+            "title": product_title,
             "image_url": img_url,
             "product_link": product_link
         }
         products.append(product_data)
 
-    return products
+    # Menyaring produk yang memiliki judul kosong dan menghitung total produk
+    products_filtered = [product for product in products if product['title']]
+    total_items = len(products_filtered)
+
+    return {"success": True, "total_items": total_items, "data": products_filtered}
+
+# Uji dengan URL yang Anda berikan
+url = "https://www.amazon.com/s?k=best+2024+laptop+deals&crid=CG1KHU2C4WB2&sprefix=best+2024+laptop%2Caps%2C945&ref=nb_sb_ss_ts-doa-p_1_16"
+result = get_amazon_products(url)
+print(result)
