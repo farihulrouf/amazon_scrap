7f44b2e52830ade5987e260d51dd7a4a561ede49
Farihul Rouf <farihulrouf@gmail.com>
2024-12-22 15:33:01 +0700

    detail

--- admin-blog/src/components/BlogDetail.vue
+++ admin-blog/src/components/BlogDetail.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="max-w-screen-xl mx-auto p-6 bg-teal-500">
+    <div class="max-w-screen-xl mx-auto p-6">
         <div class="grid grid-cols-12 gap-4 mb-4">
             <!-- Blog Title -->
             <div class="col-span-9 bg-red-500">
@@ -18,10 +18,88 @@
         <!-- Blog Image -->
         <img :src="blog.image" :alt="blog.title" class="w-full h-full object-cover rounded-lg mb-6 shadow-lg" />
 
-        <!-- Blog Content -->
-        <div class="prose prose-lg max-w-none text-gray-700">
-            <p v-html="blog.content"></p>
+        <div class="flex">
+            <div class="max-w-3xl">
+                <!-- Blog Content -->
+                <div class="prose prose-lg max-w-3xl text-gray-700 p-4">
+                    <h class="text-2xl">{{ blog.content }}</h>
+                </div>
+
+                <div class="text-gray-700 mt-6 mb-4 p-4">
+                    <h class="text-xl">
+                        {{ blog.content_x }}
+                    </h>
+                </div>
+
+                <!-- Combined Card with Image and Content -->
+                <div class="bg-white rounded-lg shadow-lg overflow-hidden mt-6">
+                    <img src="https://cdn0.vox-cdn.com/hermano/verge/product/image/10340/DSC_1142.jpg"
+                        alt="Additional Image" class="w-full h-128 object-cover" />
+                    <div class="p-6 grid grid-cols-2 gap-4">
+                        <!-- The Good Section -->
+                        <div class="flex flex-col justify-between h-full bg-green-100 p-4 rounded-lg">
+                            <h3 class="text-lg font-bold text-gray-800 mb-2">The Good</h3>
+                            <ul class="list-disc list-inside text-gray-700">
+                                <li>Above-average battery life for a gaming laptop</li>
+                                <li>Thin and light</li>
+                                <li>Exceptional screen and speakers</li>
+                                <li>All-around good build quality with little flex</li>
+                            </ul>
+                            <div class="mt-auto">
+                                <button
+                                    class="px-4 py-2 bg-green-600 text-white font-bold rounded-lg shadow hover:bg-green-500 w-full">
+                                    $2100 at Best Buy
+                                </button>
+                            </div>
+                        </div>
+                        <!-- The Bad Section -->
+                        <div class="flex flex-col justify-between h-full bg-red-100 p-4 rounded-lg">
+                            <h3 class="text-lg font-bold text-gray-800 mb-2">The Bad</h3>
+                            <ul class="list-disc list-inside text-gray-700">
+                                <li>Expensive, undercut by Asus’ own offerings</li>
+                                <li>Still runs hotter than competitors</li>
+                                <li>Performance lags behind laptops with similar specifications</li>
+                                <li>RGB lighting underwhelming for a laptop this expensive</li>
+                            </ul>
+                            <div class="mt-auto">
+                                <button
+                                    class="px-4 py-2 bg-red-600 text-white font-bold rounded-lg shadow hover:bg-red-500 w-full">
+                                    $2080 at Asus
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="text-gray-700 mt-6 mb-4 p-4">
+                    <h class="text-lg">
+                        {{ blog.content_y }}
+                    </h>
+                </div>
+
+                <div class="text-gray-700 p-4">
+                    <h class="text-lg">
+                        {{ blog.content_z }}
+                    </h>
+                </div>
+                <div class="text-gray-700 mb-4 p-4">
+                    <h class="text-lg">
+                        {{ blog.content_r }}
+                    </h>
+                </div>
+            </div>
+            <!-- List of Other Blog Posts -->
+            <div class="max-w-sm ml-6">
+                <h2 class="text-xl font-bold text-gray-800 mb-4">Most Popular</h2>
+                <div v-for="(item, index) in blogs" :key="index" class="mb-4">
+                    <div class="bg-gray-100 p-4 rounded-lg shadow">
+                        <h3 class="font-semibold text-gray-800">{{ item.title }}</h3>
+                        <p class="text-gray-600 text-sm mt-2">{{ item.date }}</p>
+                    </div>
+                </div>
+            </div>
         </div>
+
     </div>
 </template>
 
@@ -32,11 +110,36 @@ export default {
         return {
             blog: {
                 title: "Asus ROG Zephyrus G16 (2024) review: an AMD upgrade",
+                content_r: 'I have a solid basis for comparing the two G16s here. In search of a portable work machine I could also game on, I ditched my desktop and bought the Intel version of the G16, with an RTX 4070 and 16GB of RAM, in August. Since then, I’ve used it not only to work at home and in coffee shops but also to play all the games I’d normally play on my tower.',
                 image: "https://duet-cdn.vox-cdn.com/thumbor/0x0:2040x1360/2400x1600/filters:focal(1239x649:1240x650):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/25696652/DSC_1121_Enhanced_NR.jpg",
-                content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Curabitur ac faucibus orci. Nulla facilisi. Proin tincidunt sem a 
-            tortor sollicitudin, nec condimentum eros pretium.`,
+                content: `Asus overhauled its ROG Zephyrus gaming laptops in 2024, giving them sleek, 
+                aluminum bodies and brilliant OLED screens. The Intel version of the ROG Zephyrus G16 looked like a potential MacBook Pro killer when it launched in February; it is thin, light, and powerful, but the Core Ultra 9 185H processor hampers battery life. Now that Asus has reissued the G16 with the latest flagship CPU from AMD, it has finally become an all-rounder with enough battery life to tote to the office..`,
+                content_x: 'This version of the Zephyrus G16 has the same Ryzen AI 9 HX 370 CPU that powers Asus’ Zenbook S 16 to great effect. The better integrated graphics and improvements in efficiency and raw horsepower over Intel’s mobile Meteor Lake processors make the AMD model the clear winner. If only it weren’t so dang expensive. ',
+                content_y: 'Our $2,300 review unit, with an Nvidia RTX 4070 mobile GPU, is one of four G16 variants with the Ryzen AI 9 HX 370 CPU. There’s another 4070 model in white, with 32GB of RAM and 2TB of storage for $2,300, while the least expensive version is $1,900 with an RTX 4060, 16GB of RAM, and 1TB of storage. More GPU power is only available on Intel models, with an RTX 4080 option at $2,700 and an RTX 4090 for $3,300. RAM is soldered to the motherboard, so it’s important to consider before purchasing',
+                content_z: 'Apart from the processor, and the jump to Wi-Fi 7 from Wi-Fi 6e, the AMD version of the G16 is nearly identical to the Intel version. Both use the same 0.59-inch-thick unibody aluminum chassis, ports, and 200-watt charging brick; the only visible difference is the Copilot key on the AMD model in place of the right-hand Ctrl key. The AMD version is also an ounce lighter at a scant 3.99 pounds. '
             },
+            blogs: [
+                {
+                    title: "Google’s counteroffer to the government trying to break it up is unbundling Android apps",
+                    date: "Dec 21, 2024"
+                },
+                {
+                    title: "2025 will be the year of the smart lock",
+                    date: "Dec 19, 2024"
+                },
+                {
+                    title: "Three of the biggest US banks are facing a lawsuit for ‘widespread fraud’ on Zelle",
+                    date: "Dec 18, 2024"
+                },
+                {
+                    title: "ModRetro Chromatic review: an arms dealer’s Game Boy is among the best ever made",
+                    date: "Dec 17, 2024"
+                },
+                {
+                    title: "Qualcomm wins a legal battle over Arm chip licensing",
+                    date: "Dec 16, 2024"
+                }
+            ]
         };
     },
 };
